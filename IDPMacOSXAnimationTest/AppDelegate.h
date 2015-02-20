ae7944bad7819d75ca86b1a33b2f2fdc5954ffb6
Artem Chabanniy <artem.chabanniy@gmail.com>
2015-02-20 17:20:11 +0200

    Initial Commit

---
+++ IDPMacOSXAnimationTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  IDPMacOSXAnimationTest
+//
+//  Created by Artem Chabanniy on 2/20/15.
+//  Copyright (c) 2015 IDAP Group. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ IDPMacOSXAnimationTest/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  IDPMacOSXAnimationTest
+//
+//  Created by Artem Chabanniy on 2/20/15.
+//  Copyright (c) 2015 IDAP Group. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
